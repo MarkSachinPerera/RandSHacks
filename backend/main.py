075b22904133d00251c0a2a95aca82b6df214205
MarkSachinPerera <marksachin12@gmail.com>
2021-02-14 08:53:49 -0700

    login works, myspoof is an object

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, jsonify, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
 from datetime import datetime
-import spoof
+from spoof import Spoof
 from flask_cors import CORS, cross_origin
 from collections.abc import Iterable 
 
@@ -16,7 +16,7 @@ CORS(app)
 db = SQLAlchemy(app)
 from models import model
 db.create_all()
-
+myspoof = Spoof()
 # app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
@@ -25,22 +25,26 @@ def upload():
 
 @app.route('/feed', methods=['GET'])
 def get_feed_update():
-    return ( jsonify({'Feed' : spoof.get_status()}) )
+    return ( jsonify({'Feed' : myspoof.get_status()}) )
 
 @app.route('/leaderboard', methods=['GET'])
 def get_leaderboard():
-    return(jsonify ( {'Leaderboard' : spoof.get_leaderboard() } ))
+    return(jsonify ( {'Leaderboard' : myspoof.get_leaderboard() } ))
 
 
 ###########POST###############################
 @app.route('/register/<name>,<email>,<password>', methods=['GET'])
 def post_register(name, password, email):
-    try:
-        new_user = model.Users(name=name, password=password, email=email)
-        db.session.add(new_user)
-        db.session.commit()
-    except exc.IntegrityError as e:
-        return("u entered same user twice")
+    # try:
+    #     new_user = model.Users(name=name, password=password, email=email)
+    #     db.session.add(new_user)
+    #     db.session.commit()
+    # except exc.IntegrityError as e:
+    #     return("u entered same user twice")
+    # return redirect('/')
+    myspoof.userInfo['name'] = name
+    myspoof.userInfo['password'] = password
+    myspoof.userInfo['email'] = email
     return redirect('/')
 
 
@@ -62,7 +66,7 @@ def challenges_update(user_id, challenge_id, status):
 def get_all_prizes():
 
     
-    prizeList = spoof.create_prizes()
+    prizeList = myspoof.create_prizes()
 
     return (jsonify ( { 'Prizes' :  prizeList} ) )
 
@@ -81,25 +85,36 @@ def challenges_current(user_id):
     return ( jsonify( { 'Current' : data}))
 
 ###################Full user functions ########################
+
+@app.route('/user/auth/<email>,<password>')
+def auth_user(email,password):
+    # user = model.Users.query.filter_by(name=name).first_or_404()
+    # return user.name
+
+    if email == myspoof.userInfo['email'] and password == myspoof.userInfo['password']:
+        return redirect('/')
+    else:
+        return('no')
+
 @app.route('/user/name')
 def show_user():
     # user = model.Users.query.filter_by(name=name).first_or_404()
     # return user.name
 
-    return ( jsonify ( {'Name' : spoof.get_user_name()} ) )
+    return ( jsonify ( {'Name' : myspoof.get_user_name()} ) )
 
 @app.route('/user/score')
 def get_user_score():
     # user = model.Users.query.filter_by(name=name).first_or_404()
     # return user.name
 
-    return ( jsonify ( {'Score' : spoof.get_user_score()} ) )
+    return ( jsonify ( {'Score' : myspoof.get_user_score()} ) )
 
 @app.route('/user/friends')
 def get_user_friends():
     data = {}
-    fulllist = spoof.get_leaderboard()
-    friendlist = spoof.get_user_friends()
+    fulllist = myspoof.get_leaderboard()
+    friendlist = myspoof.get_user_friends()
 
     for i in friendlist:
         data[i] = {
