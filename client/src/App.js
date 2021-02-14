4ae6ec0e53c906593a6216c2a1af57af08e84841
rohitkadhe <rkadhe99@gmail.com>
2021-02-13 20:46:20 -0700

    Home page done

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Routes from './routes/routes';
 
 import './App.css';
+import NavBar from './components/navigation/NavBar';
 
 function App({ history }) {
   return (
     <div id="App">
+      <NavBar history={history} name="Current Challenges" />
       <Routes />
     </div>
   );
--- client/src/components/ChallengeCard/ChallengeCard.js
+++ client/src/components/ChallengeCard/ChallengeCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 
-export default function ChallengeCard({ onClick, color }) {
+export default function ChallengeCard({ onClick, color, image }) {
   return (
-    <Card onClick={onClick} color={color}>
-      <Image src={'Some source'} wrapped ui={false} />
+    <Card fluid centered onClick={onClick} color={color}>
+      <Image src={image} />
       <Card.Content>
         <Card.Header>{'Some data'}</Card.Header>
         <Card.Meta>Created on: {'Some date'}</Card.Meta>
