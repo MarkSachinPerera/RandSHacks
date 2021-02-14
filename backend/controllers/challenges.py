3d9d2a34418954af8210092ed8fb9cd0a8853f2f
DevonVipond <dv@DESKTOP-E8B2M59.localdomain>
2021-02-13 20:14:53 -0700

    challenges controller template

---
+++ backend/controllers/challenges.py
@@ -0,0 +1,16 @@
+# @return Array<Challenges>
+def getAllChallenges(userId):
+    pass
+
+# @return Challenge
+def getChallenge(userId, challengeId):
+    pass
+
+# @return true if successful
+def startChallenge(userId, challengeId):
+    pass
+
+# @return true if successful
+def completeTask(userId, challengeId, taskId):
+    pass
+
